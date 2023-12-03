299661098126af61421402caa773682e1eadc7bb
kferrerux <ferrerkevin@hotmail.fr>
2023-12-03 17:54:19 +0100

    various fix

---
+++ main.js
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------
+// const inputName = document.getElementById("name");
+
+// console.log(inputName);
+
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.top = e.pageY + "px";
+//   cursor.style.left = e.pageX + "px";
+
+//   cercle2.style.top = e.pageY + "px";
+//   cercle2.style.left = e.pageX + "px";
+
+//   cercle3.style.top = e.pageY + "px";
+//   cercle3.style.left = e.pageX + "px";
+// });
